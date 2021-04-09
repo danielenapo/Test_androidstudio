6c888cdd27682d5cff150bc09749d990f19a24c2
danielenapo <gnele2000@gmail.com>
2021-04-09 22:13:21 +0200

    più provola

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -17,18 +17,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button pulsanteProvola = findViewById(R.id.button2); //definisco il pulsante, tipo javascript
+        Button pulsanteProvola =(Button) findViewById(R.id.button2); //definisco il pulsante, tipo javascript
         TextView scritta= (TextView) findViewById(R.id.text2);
 
         pulsanteProvola.setOnClickListener(new View.OnClickListener() {
-            public void onClick (View pulsanteProvola) {
+            public void onClick (View pulsanteProvola) { //OVERRIDE DI ONCLICK
                 // Code here executes on main thread after user presses button
-                if (!isProvola) {
-                    scritta.setText("sei una provola");
-                    isProvola = true;
-                } else {
-                    scritta.setText("non sei una provola");
-                    isProvola = false;
+                Button pulsanteProvola2 =(Button) findViewById(R.id.button2);
+                if (!isProvola) { //prima volta click
+                    scritta.setText("Complimenti, adesso sei una provola!\n\nAspetta!! forse sei una mozzarella di bufala,\ndobbiamo fare un controllo");
+                    pulsanteProvola2.setText("Verifica la tua provolosità");
+                    isProvola=true;
+                } else {//dal secondo click in poi
+                    scritta.setText("Ok sì, sei proprio una provola.\n\nSTATO:\nsei una provola");
+                    pulsanteProvola2.setVisibility(View.GONE);
                 }
             }
         });
