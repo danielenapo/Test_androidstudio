1a678972eeff163481924055442a2b62bc23ec5f
danielenapo <gnele2000@gmail.com>
2021-05-07 10:04:43 +0200

    Bottoni onClick XML

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -18,29 +18,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
 
-        Button pulsanteProvola =(Button) findViewById(R.id.button2); //definisco il pulsante, tipo javascript
+    //METODO CHE SI ATTIVA CON OnCLick SUL PULSANTE (scritto su XML)
+    public void provolize(View v){
+        Button pulsanteProvola =(Button) findViewById(R.id.button2);
         TextView scritta= (TextView) findViewById(R.id.text2);
-
-        //METTO UN LISTENER PER IL PULSANTE DELLA PROVOLA CHE CAMBIA I TESTI
-        pulsanteProvola.setOnClickListener(new View.OnClickListener() {
-            public void onClick (View pulsanteProvola) { //OVERRIDE DI ONCLICK
-                // Code here executes on main thread after user presses button
-                Button pulsanteProvola2 =(Button) findViewById(R.id.button2);
-                if (!isProvola) { //prima volta click
-                    scritta.setText("Complimenti, adesso sei una provola!\n\nAspetta!! forse sei una mozzarella di bufala,\ndobbiamo fare un controllo");
-                    pulsanteProvola2.setText("FATTI UNA FOTO PER VERIFICARE LA TUA PROVOLOSITA'");
-                    isProvola=true;
-                }
-                else{
-                    Intent intent = new Intent(MediaStore.INTENT_ACTION_STILL_IMAGE_CAMERA);
-                    startActivity(intent);
-                    scritta.setText("Ok sì, sei proprio una provola.\n\nSTATO:\nsei una provola");
-                    pulsanteProvola2.setVisibility(View.GONE);
-                }
-            }
-        });
-
+        if (!isProvola) { //prima volta click
+            scritta.setText("Complimenti, adesso sei una provola!\n\nAspetta!! forse sei una mozzarella di bufala,\ndobbiamo fare un controllo");
+            pulsanteProvola.setText("FATTI UNA FOTO PER VERIFICARE LA TUA PROVOLOSITA'");
+            isProvola=true;
+        }
+        else{
+            Intent intent = new Intent(MediaStore.INTENT_ACTION_STILL_IMAGE_CAMERA);
+            startActivity(intent);
+            scritta.setText("Ok sì, sei proprio una provola.\n\nSTATO:\nsei una provola");
+            pulsanteProvola.setVisibility(View.GONE);
+        }
     }
 
 }
